a5a130388ddb0cf6f5c1751edb01717fc887c645
arron_taylor <arron.taylor@moody.edu>
2020-12-26 18:02:26 -0600

    add idbadge icon for account level

--- admin/src/components/Avatar.js
+++ admin/src/components/Avatar.js
@@ -1,5 +1,7 @@
 import style from '../styles.module.css';
 import { gql, useQuery } from "@apollo/client";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faIdBadge } from '@fortawesome/free-solid-svg-icons'
 
 export default function Avatar(props) {
 	
@@ -7,7 +9,7 @@ export default function Avatar(props) {
 		<div>
 			<div className={style.avatar} />
 			<div className={style.fullname}> {props.user.name} </div>
-			<div className={style.accountlevel}> Full Access Account </div>
+			<div className={style.accountlevel}> <FontAwesomeIcon className={ style.icon } icon={ faIdBadge } /> Full Access Account </div>
 		</div>
 	)
 }
