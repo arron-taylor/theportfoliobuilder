a5686a6e092a3c96fc1b2d34ab209d7525dc9092
arron_taylor <arron.taylor@moody.edu>
2021-01-02 11:09:04 -0600

    edit the no access error page

--- admin/src/pages/NoAccess.js
+++ admin/src/pages/NoAccess.js
@@ -12,7 +12,7 @@ export default function NoAccess(props) {
 
   return (
     <div className={error.accesscontainer}> 
-        <h1> Ohmm.... </h1>
+        <h1> Ummm.... </h1>
         <img src='./frog.png' />
         <h2> Well this is weird. <br /> It looks like you aren't allowed to access the page you were looking for. </h2>
         <h3> Go back to <Link to='/login'> theportfoliobuilder.com </Link> or contact us about a problem. </h3>
